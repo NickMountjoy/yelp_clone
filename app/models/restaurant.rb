7d9f6e89cb52768992ec52f311d27eef960ae193
NickMountjoy <nick.mountjoy98@gmail.com>
2016-04-15 12:45:19 +0100

    Added images locally

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,9 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   validates :name, length: {minimum: 3}, uniqueness: true
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def belongs_to_user?(restaurant, user)
     restaurant.user_id == user.id
   end
